7c4788c5ecc32fb803cbd8fd068f01bfafae90ff
Zarytskyi Oleksandr <pfantom180@gmail.com>
2026-06-02 17:47:16 +0300

    task is done!

---
+++ src/main/java/core/basesyntax/impl/Box.java
@@ -0,0 +1,26 @@
+package core.basesyntax.impl;
+
+public class Box<K, V> {
+
+    private K keys;
+    private V values;
+
+    public K getKey() {
+        return keys;
+    }
+
+    public V getValue() {
+        return values;
+    }
+
+    public void setValues(V values) {
+        this.values = values;
+    }
+
+    public Box(K key, V values) {
+        this.keys = key;
+        this.values = values;
+
+    }
+
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_ITEMS_NUMBER = 10;
+
+    private int size;
+
+    private Box<K, V>[] items;
+
+    public StorageImpl() {
+        items = new Box[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+
+        for (int i = 0; i < size; i++) {
+            if (items[i].getKey().equals(key)) {
+                items[i].setValues(value);
+                return;
+            }
+        }
+
+        items[size++] = new Box<>(key, value);
+
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].getKey().equals(key)) {
+                return items[i].getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
